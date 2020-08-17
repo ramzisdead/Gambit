1ee151a7b1d7cfe97ad8d117d8b6f0dfda4eb754
ramzisdead <ramzisdead@gmail.com>
2020-08-17 20:58:23 +0300

    Initial Commit

---
+++ Gambit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gambit
+//
+//  Created by Рамазан on 17.08.2020.
+//  Copyright © 2020 Рамазан. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
